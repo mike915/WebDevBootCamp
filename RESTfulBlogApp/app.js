63d4cf0e26ee8823c0e699d65a785b9072dbc5dc
Michael Xiao <michael.xiao.xiang@gmail.com>
2018-11-08 02:33:37 +0000

    RESTful Blog App: EDIT and UPDATE

--- RESTfulBlogApp/app.js
+++ RESTfulBlogApp/app.js
@@ -1,13 +1,16 @@
 let express = require("express");
 let bodyParser = require("body-parser");
 let mongoose = require("mongoose");
+let methodOverride = require("method-override");
 
 let app = express();
 mongoose.connect('mongodb://localhost:27017/restful_blog_app', { useNewUrlParser: true }); 
+mongoose.set('useFindAndModify', false);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(methodOverride('_method'));
 
 let blogSchema = new mongoose.Schema({
     title: String,
@@ -71,6 +74,28 @@ app.get('/blogs/:id', function(req, res) {
             res.redirect('/blogs');
         } else {
             res.render('show', {blog: foundBlog});
+        }
+    });
+});
+
+// EDIT
+app.get('/blogs/:id/edit', function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if(err) {
+            res.redirect('blogs');
+        } else {
+            res.render('edit', {blog: foundBlog});
+        }
+    });
+});
+
+// UPDATE
+app.put('/blogs/:id', function(req, res) {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updateBlog) {
+        if(err) {
+            res.redirect('/blogs');
+        } else {
+            res.redirect('/blogs/' + req.params.id);
         }
     });
 });
