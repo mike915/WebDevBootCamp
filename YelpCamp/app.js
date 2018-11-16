cee506c5f220bd6842dba9f07459748c342c6223
Michael Xiao <michael.xiao.xiang@gmail.com>
2018-11-16 01:38:12 +0000

    YelpCamp: Creating Comments Pt.1

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -22,7 +22,7 @@ app.get('/campgrounds', (req, res) => {
         if(err) {
             console.log(err);
         } else {
-            res.render('index', {campgrounds: allCampgrounds});
+            res.render('campgrounds/index', {campgrounds: allCampgrounds});
         }
     });
 });
@@ -48,7 +48,7 @@ app.post('/campgrounds', (req, res) => {
 });
 
 app.get('/campgrounds/new', (req, res) => {
-    res.render('new');
+    res.render('campgrounds/new');
 });
 
 app.get('/campgrounds/:id', (req, res) => {
@@ -58,10 +58,21 @@ app.get('/campgrounds/:id', (req, res) => {
             console.log(err);
         } else {
             console.log(foundCampground);
-            res.render('show', {campground: foundCampground});
+            res.render('campgrounds/show', {campground: foundCampground});
         }
     });
 });
+
+// ===========
+app.get('/campgrounds/:id/comments/new', function(req, res) {
+    Campground.findById(req.params.id, function(err, campground) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('comments/new', {campground: campground});
+        }
+    });
+})
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log(`The YelpCamp Server Has Started ${process.env.IP}:${process.env.PORT}`);
