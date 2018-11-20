c46257932ac363af4739020db631c1f72dbb7033
Michael Xiao <michael.xiao.xiang@gmail.com>
2018-11-20 02:15:16 +0000

    Secret Page Pt.2

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,12 +1,29 @@
 let express = require("express");
 let bodyParser = require("body-parser");
 let mongoose = require("mongoose");
+let passport = require("passport");
+let LocalStrategy = require("passport-local");
+let passportLocalMongoose = require("passport-local-mongoose");
+let User = require("./models/user");
+
+
 
 let app = express();
 mongoose.connect('mongodb://localhost:27017/auth_demo_app', { useNewUrlParser: true }); 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser);
+passport.deserializeUser(User.deserializeUser);
+
+app.use(require("express-session")({
+    secret: 'Rusty is the best and custet dog in the world',
+    resave: false,
+    saveUninitialized: false
+}));
 
 app.get('/', function(req, res) {
     res.render('home');
