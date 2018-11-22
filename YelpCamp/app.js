5639ca78640ffe6da0ecb378f06da3480661d877
Michael Xiao <michael.xiao.xiang@gmail.com>
2018-11-22 02:20:45 +0000

    YelpCamp: Adding Auth Pt.4

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -30,6 +30,11 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
     
 app.get('/', (req, res) => {
     res.render('landing');
