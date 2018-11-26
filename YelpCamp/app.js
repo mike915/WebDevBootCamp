f26c72574e0421b0151b489f8082cc27d01e593b
Michael Xiao <michael.xiao.xiang@gmail.com>
2018-11-26 02:36:57 +0000

    YelpCamp: User Associations: Comment

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
-seedDB();
+// seedDB();
 
 // Passport configuration
 app.use(require("express-session")({
--- YelpCamp/models/comment.js
+++ YelpCamp/models/comment.js
@@ -2,7 +2,13 @@ let mongoose = require("mongoose");
  
 let commentSchema = new mongoose.Schema({
     text: String,
-    author: String
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        username: String
+    }
 });
  
 module.exports = mongoose.model("Comment", commentSchema);
