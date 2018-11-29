96a12b10852cfaa583d04983c9c21c81806ea474
Michael Xiao <michael.xiao.xiang@gmail.com>
2018-11-29 00:16:55 +0000

    Campground Authorization Pt.2

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -61,7 +61,7 @@ router.get('/:id/edit', checkCampgroundOwnership, function(req, res) {
     });        
 });
 
-router.put('/:id', function(req, res) {
+router.put('/:id', checkCampgroundOwnership, function(req, res) {
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
         if(err) {
             res.redirect('/campgrounds');
@@ -71,7 +71,7 @@ router.put('/:id', function(req, res) {
     });
 });
 
-router.delete('/:id', function(req, res) {
+router.delete('/:id', checkCampgroundOwnership, function(req, res) {
     Campground.findByIdAndRemove(req.params.id, function(err) {
         if(err) {
             res.redirect('/campgrounds');
