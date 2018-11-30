8ad36ee00fc513890ac796a84a7496c49979ca10
Michael Xiao <michael.xiao.xiang@gmail.com>
2018-11-30 02:06:55 +0000

    Comment Edit and Update

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -37,6 +37,26 @@ router.post('/', isLoggedIn, function(req, res) {
     });
 });
 
+router.get('/:comment_id/edit', function(req, res) {
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+        if(err) {
+            res.redirect('back');
+        } else {
+            res.render('comments/edit', {campground_id: req.params.id, comment: foundComment});
+        }
+    });
+});
+
+router.put('/:comment_id', function(req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+        if(err) {
+            res.redirect('back');
+        } else {
+            res.redirect('/campgrounds/' + req.params.id);
+        }
+    });
+});
+
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
