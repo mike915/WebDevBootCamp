82d9af1d1f3c55f3eb7579bd7ec5d3b3573f9a47
Michael Xiao <michael.xiao.xiang@gmail.com>
2018-12-05 01:22:45 +0000

    Comment Authorization

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -37,7 +37,7 @@ router.post('/', isLoggedIn, function(req, res) {
     });
 });
 
-router.get('/:comment_id/edit', function(req, res) {
+router.get('/:comment_id/edit', checkCommentOwnership, function(req, res) {
     Comment.findById(req.params.comment_id, function(err, foundComment) {
         if(err) {
             res.redirect('back');
@@ -47,7 +47,7 @@ router.get('/:comment_id/edit', function(req, res) {
     });
 });
 
-router.put('/:comment_id', function(req, res) {
+router.put('/:comment_id', checkCommentOwnership, function(req, res) {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
         if(err) {
             res.redirect('back');
@@ -57,7 +57,7 @@ router.put('/:comment_id', function(req, res) {
     });
 });
 
-router.delete('/:comment_id', function(req, res) {
+router.delete('/:comment_id', checkCommentOwnership, function(req, res) {
     Comment.findByIdAndRemove(req.params.comment_id, function(err) {
         if(err) {
             res.redirect('back');
@@ -72,6 +72,25 @@ function isLoggedIn(req, res, next) {
         return next();
     }
     res.redirect('/login');
+}
+
+function checkCommentOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if(err) {
+                res.redirect('back');
+            } else {
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect('back');
+                }
+            }
+        });        
+    } else {
+        console.log('YOU NEED TO BE LOGGED IN TO DO THAT!!!');
+        res.redirect('back');
+    }
 }
 
 module.exports = router;
