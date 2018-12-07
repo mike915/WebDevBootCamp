18e4bb700dcc9d748bf9928c0e3ece5f4f24722e
Michael Xiao <michael.xiao.xiang@gmail.com>
2018-12-07 01:40:13 +0000

    Flash Message: Helpful Errors

--- YelpCamp/middleware/index.js
+++ YelpCamp/middleware/index.js
@@ -45,6 +45,7 @@ middlewareOjb.isLoggedIn = function(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
+    req.flash('error', 'Please Login First!');
     res.redirect('/login');
 }
 
--- YelpCamp/routes/index.js
+++ YelpCamp/routes/index.js
@@ -38,14 +38,8 @@ router.post('/login', passport.authenticate('local',
 
 router.get('/logout', function(req, res) {
     req.logout();
+    req.flash('success', 'Logged you out!')
     res.redirect('/campgrounds');
 });
-
-function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect('/login');
-}
 
 module.exports = router;
